c262ec0564812667e99f89363ae0fc307399b939
Salomé Conde <salomecj@gmailcom>
2024-04-24 11:53:07 +0200

    labw5

--- js/index.js
+++ js/index.js
@@ -1,24 +1,54 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+ 
+  const subtotalElement = product.querySelector('.subtotal span');
+  const priceElement = product.querySelector('.price span');
+  const quantityElement = product.querySelector('.quantity input');
 
-  //... your code goes here
+
+  const price = parseFloat(priceElement.innerHTML);
+  const quantity = parseInt(quantityElement.value);
+
+  
+  const subtotal = price * quantity;
+
+  
+  subtotalElement.textContent = `$${subtotal.toFixed(2)}`;
+
+  
+  return subtotal;
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
+  
+  function calculateAll() {
+    
+    const productRows = document.getElementsByClassName('product');
+  
+    for (const productRow of productRows) {
+      updateSubtotal(productRow);
+    }
+  }
+  
 
   // ITERATION 3
-  //... your code goes here
-}
+  function calculateAll() {
+    const subtotals = [];
+    const productRows = document.getElementsByClassName('product');
+
+    for (const productRow of productRows) {
+      const subtotal = updateSubtotal(productRow);
+      subtotals.push(subtotal); 
+    }  
+    const totalPrice = subtotals.reduce((acc, curr) => acc + curr, 0);
+
+    const totalElement = document.getElementById('total-value');
+    totalElement.textContent = `$${totalPrice.toFixed(2)}`;
+  }
+
+
 
 // ITERATION 4
 
